61831df0ca66b7afadacea029867bcfe98c493a3
Quentin Russell <quentin.russell@gmail.com>
2026-06-22 10:57:12 -0400

    updated build files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '61cc288acbe73307255e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'f6202cb7288c74eb2824');
